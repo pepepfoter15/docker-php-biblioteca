d43929299fe383e4fc3e8e9ee5e1c3547afa0785
José Carlos Rodríguez Cañas <119460713+pepepfoter15@users.noreply.github.com>
2024-02-19 10:23:56 +0100

    Todo el contenido

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+define('base_url',getenv('BASE_URL'));
+define('host',getenv('DB_HOST'));
+define('user',getenv('DB_USER'));
+define('pass',getenv('DB_PASS'));
+define('db',getenv('DB_NAME'));
+const charset = "charset=utf8";
+?>
